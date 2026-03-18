865e4ffd3c835da0dab69e350eec2e3d082de5dd
s6752410004@sau.ac.th <s6752410004@sau.ac.th>
2026-03-19 00:37:51 +0700

    v2

--- lib/car_cal.dart
+++ lib/car_cal.dart
@@ -10,6 +10,6 @@ class CarCal extends StatefulWidget {
 class _CarCalState extends State<CarCal> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
