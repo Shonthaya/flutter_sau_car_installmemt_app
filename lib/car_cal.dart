9c7c5717bdcb1cbcd539bfe98eb4ddcb78893209
s6752410004@sau.ac.th <s6752410004@sau.ac.th>
2026-03-19 00:23:58 +0700

    FirstOne

---
+++ lib/car_cal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CarCal extends StatefulWidget {
+  const CarCal({super.key});
+
+  @override
+  State<CarCal> createState() => _CarCalState();
+}
+
+class _CarCalState extends State<CarCal> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_awsome_app/car_cal.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const FlutterSauCarInstallmemetApp());
+}
+
+// ================================================
+class FlutterSauCarInstallmemetApp extends StatefulWidget {
+  const FlutterSauCarInstallmemetApp({super.key});
+
+  @override
+  State<FlutterSauCarInstallmemetApp> createState() =>
+      _FlutterSauCarInstallmemetAppState();
+}
+
+class _FlutterSauCarInstallmemetAppState
+    extends State<FlutterSauCarInstallmemetApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CarCal(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+    );
+  }
+}
